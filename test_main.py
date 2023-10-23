0465c95aeecda12ed9d57701fcf935e2426a03b4
FizaAamir <144325138+FizaAamir@users.noreply.github.com>
2023-10-23 23:08:34 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,18 +1,14 @@
-from main import wallet
+from main import Wallet
 
-balance = wallet()
+balance = Wallet()
 
-#here we made 3 different test cases
-def addBalance():
-    balance.addBalance(100)
-    assert balance.addBalance()==100
+def test_add_balance():  # Function names should start with 'test_'
+    balance.add_balance(100)
+    assert balance.get_balance() == 100
 
-def removeBalance():
-    balance.removeBalance(100)
-    assert balance.removeBalance()==0
+def test_remove_balance():  # Function names should start with 'test_'
+    balance.remove_balance(100)
+    assert balance.get_balance() == 0
 
-def checkBalance():
-    assert balance.getBalance() == 0 
-
-
-#after making test cases, we need to push to GitHub
+def test_check_balance():  # Function names should start with 'test_'
+    assert balance.get_balance() == 0
